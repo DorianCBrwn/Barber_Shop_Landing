c2bcdf194ba5add17b107bda7d9fc0040e4445f4
Dorian Brown <brown.3794@gmail.com>
2022-05-18 11:39:39 -0400

    feat: :wrench: update colors for tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{svelte,js,svelte,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Spartan", "sans-serif"],
+      },
+      color: {
+        black: "#212123",
+      },
+    },
   },
   plugins: [],
 };
