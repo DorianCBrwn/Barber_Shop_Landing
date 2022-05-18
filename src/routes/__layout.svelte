503cdf9630fae5e37214d73b3fef6b614a95d7a9
Dorian Brown <brown.3794@gmail.com>
2022-05-18 11:38:36 -0400

    feat: :sparkles: Add Navbar to layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import "../app.css";
+  import Navbar from "$lib/components/Navbar.svelte";
+  import "@fontsource/spartan/400.css";
+  import "@fontsource/spartan/500.css";
+  import "@fontsource/spartan/600.css";
+  import "@fontsource/spartan/700.css";
+</script>
+
+<section class="">
+  <Navbar />
+  <slot />
+</section>
